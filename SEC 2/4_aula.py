763650cfd23a7d2e54883b4b993bfd7d8255d477
Pedro <pedro11.trevisan@gmail.com>
2025-11-27 17:19:21 -0300

    another day another victory

--- SEC 2/4_aula.py
+++ SEC 2/4_aula.py
@@ -12,10 +12,26 @@ mult = num1 * num2
 mod = num1 % num2 
 exp = num1 ** num2
 
-
 print(sum)
 print(sub)
 print(div)
 print(mult)
 print(mod)
 print(exp)
+
+# comparação 
+
+bigger = num1 > num2
+smaller = num1 < num2
+equal = num1 == num2 
+different = num1 !=  num2
+bigger_equal = num1 >= num2 
+smaller_equal = num1 <= num2
+
+print(bigger)
+print(smaller)
+print(equal)
+print(different)
+print(bigger_equal)
+print(smaller_equal)
+

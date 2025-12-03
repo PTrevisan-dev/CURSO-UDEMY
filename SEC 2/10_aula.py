e2d9b8d811c9ad943884eb006f75c2d747f34ff0
Pedro <pedro11.trevisan@gmail.com>
2025-12-03 15:45:47 -0300

    novas aulas a 1 passo do dicionario

---
+++ SEC 2/10_aula.py
@@ -0,0 +1,21 @@
+# tupla 
+
+filmlist = ('Kung fu panda', 'Interstellar', 'Intocaveis', 
+            'star vs forças do mal')
+
+print(type(filmlist))
+
+# 1- buscar os dois primeiros itens da tupla 
+print(filmlist[:2])
+
+# 2- buscar o ultimo iten da tupla 
+print(filmlist[-1])
+
+# 3- buscar filmes ate uma certa posição 
+print(filmlist[:3])
+
+# 4- buscar filmes de uma posição em diante
+print(filmlist[2:])
+
+# 5- recuperar item 
+print(filmlist.index('star vs forças do mal'))
---
+++ SEC 2/9_aula.py
@@ -0,0 +1,23 @@
+# metodos para listas 
+
+filmlist = ['Kung fu panda', 'Interstellar', 'Intocaveis', 
+            'star vs forças do mal']
+        
+# 1- tamanho da lista 
+print(len(filmlist))
+
+# 2- recuperar um iten da lista pelo nome 
+print(filmlist.index('star vs forças do mal'))
+
+# 3- adicionar item ao final da lista
+filmlist.append('Game of Thrones')
+print(filmlist)
+
+# 4- ordenas lista 
+filmlist.sort()
+print(filmlist)
+
+# 5- copiar um filme de uma lista para outra 
+filmcopy = filmlist.copy()
+filmcopy.remove('Interstellar')
+print(filmcopy)

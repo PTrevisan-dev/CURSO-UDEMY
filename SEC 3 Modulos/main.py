e99197ffe3d2858af4347f7d41b9968c4e724eca
Pedro <pedro11.trevisan@gmail.com>
2025-12-15 10:20:06 -0300

    inicio modulo 3

---
+++ SEC 3 Modulos/main.py
@@ -0,0 +1,19 @@
+# Main page for SEC 3 Modulos
+
+import math_operation
+from math_operation import multiply, divide
+
+print(math_operation.sum(4, 3))
+print(math_operation.subtract(10, 3))
+
+print(multiply(3.5, 2))
+print(divide(7, 1))
+
+'''----------------------------------------------------------------------------------------------------'''
+
+import string_utils
+
+print(string_utils.capitalize('hello world'))
+print(string_utils.reverse_string('Python'))
+print(string_utils.count('Apple'))
+
